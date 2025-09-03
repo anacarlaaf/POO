14730a94ef43484c8c0e8e5423cf63cffdf3d13a
anacarla <anacarla.daf@gmail.com>
2025-09-02 21:25:13 -0400

    união de classes

--- Entrada.java
+++
@@ -1,9 +0,0 @@
-class Entrada extends Transacao{
-
-    private enum tipoEntrada{
-        SALARIO, RENDA_EXTRA, INVESTIMENTO, PASSIVA
-    }
-
-    private tipoEntrada entrada;
-    private FonteRenda origem;
-}
--- Transacao.java
+++ Transacao.java
@@ -11,3 +11,39 @@ abstract class Transacao{
     protected Date data;
     protected String descricao;
 }
+
+class Entrada extends Transacao{
+
+    private enum tipoEntrada{
+        SALARIO, RENDA_EXTRA, INVESTIMENTO, PASSIVA
+    }
+
+    private tipoEntrada entrada;
+    private FonteRenda origem;
+}
+
+class Gasto extends Transacao{
+
+    private enum categoriaGasto{
+        ALIMENTACAO, SAUDE, MORADIA, LAZER, EDUCACAO, TRANSPORTE
+    }
+
+    private enum categoriaPagamento{
+        CREDITO, DEBITO, PIX, DINHEIRO
+    }
+
+    private categoriaGasto categoria;
+    private String origem;
+    private  categoriaPagamento formaPagamento;
+}
+
+class FonteRenda {
+
+    protected enum tipoRecorrencia{
+        MENSAL, QUIZENAL, ANUAL, ESPORADICA
+    }
+    
+    private int id;
+    private String nome;
+    private tipoRecorrencia recorrencia;
+}
