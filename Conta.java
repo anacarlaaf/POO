95ce0fef544ad1545e69133fc3bfddbb183cb29a
Ana Carla Fernandes <102979828+anacarlaaf@users.noreply.github.com>
2025-11-07 10:08:21 -0400

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,37 @@
+package minhasfinancas;
+import java.util.*;
+
+abstract class Conta<T>{
+    protected int codigo;
+    protected String nome;
+    protected int numeroConta;
+    protected double saldo;
+    protected String tokenAcesso;
+    protected List<T> transacoes;
+
+    public Conta(int codigo, String nome, int numeroConta, String tokenAcesso) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.numeroConta = numeroConta;
+        this.tokenAcesso = tokenAcesso;
+        this.transacoes = new ArrayList<>();
+    }
+
+    public String getNome() {
+      return this.nome;
+    }
+
+    public int getCodigo() {
+      return this.codigo;
+    }
+
+    public double getSaldo() {
+      return this.saldo;
+    }
+
+    public void addTransacao(T transacao){
+        this.transacoes.add(transacao);
+    }
+
+    // public abstract void sincronizar(){} // Em andamento...
+}
