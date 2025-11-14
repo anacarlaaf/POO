74dfa198fa627adac79e2ac6019ba197264094d3
Natasha Caxias <166858901+natashacaxias@users.noreply.github.com>
2025-11-14 09:33:42 -0400

    Update ContaCorretora.java

--- financas/src/minhasfinancas/ContaCorretora.java
+++ financas/src/minhasfinancas/ContaCorretora.java
@@ -38,40 +38,7 @@ public class ContaCorretora extends Conta<Investimento> {
      * Se houver, cada investimento é exibido utilizando seu método
      * {@link Investimento#mostrar()}.</p>
      */
-    public void listarInvestimentos() {package minhasfinancas;
-import java.util.*;
-
-public class ContaCorretora extends Conta<Investimento> {
-
-    public ContaCorretora(int codigo, String nome, int numeroConta) {
-        super(codigo, nome, numeroConta); // Chama o construtor da classe pai
-    }
-
-    public void addInvestimento(Investimento var1) {
-        this.transacoes.add(var1);
-    }
-
     public void listarInvestimentos() {
-        if (this.transacoes.isEmpty()) {
-            System.out.println("Nenhum investimento cadastrado na corretora " + this.nome);
-        } else {
-            System.out.println("Investimentos na corretora " + this.nome + ":");
-            Iterator var1 = this.transacoes.iterator();
-
-            while(var1.hasNext()) {
-                Investimento var2 = (Investimento)var1.next();
-                var2.mostrar();
-            }
-        }
-    }
-
-    @Override
-    public void sincronizar() {
-        // Em construção
-    }
-}
-
-
         if (this.transacoes.isEmpty()) {
             System.out.println("Nenhum investimento cadastrado na corretora " + this.nome);
         } else {
